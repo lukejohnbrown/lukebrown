7f29fba436638b4904f9d252ad52e8121bea954e
lukejohnbrown@gmail.com <lukejohnbrown@gmail.com>
2020-03-27 17:29:28 +0000

    Contact form API

---
+++ api/src/mailer.js
@@ -0,0 +1,44 @@
+const nodemailer = require("nodemailer");
+
+const { GSUITE_CLIENT_ID, GSUITE_PRIVATE_KEY } = process.env;
+
+exports.handler = async ({ body }, context, callback) => {
+  const { name, email, message } = JSON.parse(body);
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      type: 'OAuth2',
+      user: "admin@lukebrown.io",
+      serviceClient: GSUITE_CLIENT_ID,
+      privateKey: GSUITE_PRIVATE_KEY,
+    },
+  });
+
+  try {
+    await transporter.verify();
+    await transporter.sendMail({
+      from: "admin@lukebrown.io",
+      to: "hello@lukebrown.io",
+      subject: 'New email',
+      text: `${email} ${name} ${message}`,
+    }, (error, info) => {
+      if (error) {
+        callback(null, {
+          statusCode: 500,
+          body: JSON.stringify(error.message),
+        });
+      }
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(info),
+      });
+    });
+  } catch (err) {
+    callback(null, {
+      statusCode: err.response.status,
+      body: "Error!"
+    });
+  }
+}
