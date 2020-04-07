250ca91e384b6e90fcaed7ab2aaad2246f0e5aff
lukejohnbrown@gmail.com <lukejohnbrown@gmail.com>
2020-04-07 07:12:54 +0100

    Add fathom tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,8 +66,14 @@ module.exports = {
         ],
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: 'gatsby-plugin-fathom',
+      options: {
+        siteId: 'PGRDHUXM',
+        whitelistHostnames: [
+          'lukebrown.io'
+        ]
+      }
+    }
   ],
 }
