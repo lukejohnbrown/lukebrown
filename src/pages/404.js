c54cb672c2b889ec728ca181c37d4a89073da961
lukejohnbrown@gmail.com <lukejohnbrown@gmail.com>
2020-04-09 08:16:04 +0100

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,39 @@
 import React from "react"
+import styled from "styled-components";
 
-import { Layout, Seo } from "../components"
+import {
+  Layout,
+  Container,
+  Seo,
+  Title,
+  Button,
+  Footer,
+} from "../components/shared"
+
+const Cat = styled.img`
+  width: 100%;
+`;
+
+const Subtitle = styled.p`
+  font-size: ${({ theme }) => theme.fontSize[200]};
+  margin-bottom: ${({ theme }) => theme.space[3]};
+`;
+
+const Disclaimer = styled.p`
+  font-size: ${({ theme }) => theme.fontSize[-100]};
+  margin-top: ${({ theme }) => theme.space[1]};
+`;
 
 const NotFoundPage = () => (
   <Layout>
-    <Seo title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Seo title="Page not found" />
+    <Container>
+      <Title>Oh dear, that page can't be found!</Title>
+      <Subtitle>Sorry about that, here is a random cat gif to make it up to you...</Subtitle>
+      <Cat src={`https://cataas.com/cat/gif?size=50&cache=${Date.now()}`} />
+      <Disclaimer>☝️ This gif truly is randomly generated, sorry if it is somehow inappropriate!</Disclaimer>
+      <Footer buttonText="Take me home" buttonLink="/" />
+    </Container>
   </Layout>
 )
 
