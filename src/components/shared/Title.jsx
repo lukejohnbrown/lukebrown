d463b5158176c5ab61f4cd34a1ad839417605312
lukejohnbrown@gmail.com <lukejohnbrown@gmail.com>
2020-04-13 16:10:20 +0100

    Increase title line height

--- src/components/shared/Title.jsx
+++ src/components/shared/Title.jsx
@@ -5,11 +5,12 @@ const TitleWrapper = styled.h1`
   font-size: ${({ theme }) => theme.fontSize[600]};
   font-weight: ${({ theme }) => theme.fontWeight[2]};
   margin: ${({ theme }) => theme.space[6]} 0 ${({ theme }) => theme.space[5]} 0;
-  line-height: 1.1;
+  line-height: 1.3;
   display: flex;
   align-items: center;
 
   @media ${({ theme }) => theme.breakpoints.up.md} {
+    line-height: 1.2;
     font-size: ${({ theme }) => theme.fontSize[600]};
     margin: ${({ theme }) => theme.space[7]} 0 ${({ theme }) => theme.space[4]} 0;
   };
