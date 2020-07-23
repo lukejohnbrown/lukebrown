66dc2d0305a69062b2bd5f1aec686a94ad7ebb35
lukejohnbrown@gmail.com <lukejohnbrown@gmail.com>
2020-07-23 07:52:08 +0100

    Fix link tag

--- src/html.js
+++ src/html.js
@@ -11,13 +11,16 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600;800&family=Source+Code+Pro&display=swap" rel="stylesheet">
+        <link
+          href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600;800&family=Source+Code+Pro&display=swap"
+          rel="stylesheet"
+        />
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
         <div
-          key={`body`}
+          key="body"
           id="___gatsby"
           dangerouslySetInnerHTML={{ __html: props.body }}
         />
